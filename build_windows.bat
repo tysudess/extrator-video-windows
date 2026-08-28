2964bd5a2ecf8b3326bfb275446b8d2f6e8f2aac
RODRIGODESS <diguinhodess@gmail.com>
2026-08-28 11:29:52 -0300

    Windows v2.0.4 - atualizar build local

--- build_windows.bat
+++ build_windows.bat
@@ -20,10 +20,10 @@ if not exist bin\ffprobe.exe goto :faltou
 if not exist bin\deno.exe goto :faltou
 if not exist ExtratorVideos-Icone.ico goto :icone
 
-py -3.12 -m py_compile main.py novo_layout.py modern_layout.py refined_layout.py refined_layout_v2.py refined_layout_v201.py refined_layout_v202.py refined_layout_v203.py range_slider.py advanced_editor.py
+py -3.12 -m py_compile main.py novo_layout.py modern_layout.py refined_layout.py refined_layout_v2.py refined_layout_v201.py refined_layout_v202.py refined_layout_v203.py refined_layout_v204.py range_slider.py advanced_editor.py
 if errorlevel 1 goto :erro
 
-py -3.12 -m PyInstaller --noconfirm --clean --windowed --onedir --name ExtratorVideos --contents-directory _internal --icon ExtratorVideos-Icone.ico refined_layout_v203.py
+py -3.12 -m PyInstaller --noconfirm --clean --windowed --onedir --name ExtratorVideos --contents-directory _internal --icon ExtratorVideos-Icone.ico refined_layout_v204.py
 if errorlevel 1 goto :erro
 
 xcopy /E /I /Y bin dist\ExtratorVideos\bin >nul
@@ -31,7 +31,7 @@ copy /Y LEIA-ME.txt dist\ExtratorVideos\LEIA-ME.txt >nul
 copy /Y ExtratorVideos-Icone.ico dist\ExtratorVideos\ExtratorVideos-Icone.ico >nul
 
 echo.
-echo Build v2.0.3 concluido em dist\ExtratorVideos
+echo Build v2.0.4 concluido em dist\ExtratorVideos
 pause
 exit /b 0
 
